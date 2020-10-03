5bf430bca4b077376a2c6adc371ad2f9cea6efaf
looping-torrent-8105 <gianherrera96@gmail.com>
2020-10-03 01:53:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // add solution here
 const  singers  =  [ "John Lennon" ,  "Ringo Starr" ,  "Paul McCartney" ,  "George Harrison" ];
 const instruments = [ "guitarra" ,  "batería" ,  "piano" ,  "bajo" ]
+
+var theBeatlesPlay = (musicians,instruments) => {
+  let action = [];
+  for (var i = 0; i < musicians.length; i++) {
+     action.push(musicians[i]+ "plays" +instruments[i]);
+  }
+  return action;
+}
