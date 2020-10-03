40cdc00a757d5bee51363818157f57240a870803
looping-torrent-8105 <gianherrera96@gmail.com>
2020-10-03 01:39:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 // add solution here
+const  singers  =  [ "John Lennon" ,  "Ringo Starr" ,  "Paul McCartney" ,  "George Harrison" ];
+const instruments = [ "guitarra" ,  "batería" ,  "piano" ,  "bajo" ]
